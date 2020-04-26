b6f81fa27b5cc1c3e0859bdf5bf091335f56f851
William Schrader <wfschrad@gmail.com>
2020-04-25 18:10:28 -0700

    Overall functionality resumed. Can place multiple markers dynamically on map.

--- public/js/initMap.js
+++ public/js/initMap.js
@@ -1,28 +1,68 @@
-// document.addEventListener('DOMContentLoaded', () => {
-// 	function initMap() {
-// 		let map = new google.maps.Map(document.getElementById('map'), {
-// 			center: { lat: 37.773, lng: -122.431 },
-// 			zoom: 13
-// 		});
-// 		console.log(map);
-// 	}
-// 	initMap();
-// });
+window.initMap = () => {
+	const sf = { lat: 37.773, lng: -122.431 };
+	const test2 = { lat: 37.777, lng: -122.418 };
+	const test3 = { lat: 37.779, lng: -122.421 };
+	const test4 = { lat: 37.772, lng: -122.414 };
 
-// Create the script tag, set the appropriate attributes
-// var map;
-// var script = document.createElement('script');
-// script.defer = true;
-// script.async = true;
-
-var map;
-// Attach your callback function to the `window` object
-window.initMap = function() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: { lat: 37.773, lng: -122.431 },
+	const options = {
+		center: sf,
 		zoom: 13
-	});
-};
+	};
+	const map = new google.maps.Map(document.getElementById('map'),
+		options);
 
-// Append the 'script' element to 'head'
-// document.head.appendChild(script);
+
+	const markers = [
+		{ coords: sf, iconImage: 'http://maps.google.com/mapfiles/ms/icons/flag.png', content: '<h3>Resto Info' },
+		{ coords: test2, iconImage: 'http://maps.google.com/mapfiles/ms/icons/flag.png', content: '<h3>Resto Info' },
+		{ coords: test3, iconImage: 'http://maps.google.com/mapfiles/ms/icons/flag.png', content: '<h3>Resto Info' },
+		{ coords: test4, iconImage: 'http://maps.google.com/mapfiles/ms/icons/flag.png', content: '<h3>Resto Info' }
+	];
+
+	markers.forEach((props) => {
+		addMarker(props);
+	});
+	// addMarker({
+	//     coords: sf,
+	//     iconImage: 'http://maps.google.com/mapfiles/ms/icons/flag.png',
+	//     content: '<h3>Resto Info'
+	// });
+	// addMarker({
+	//     coords: test2
+	// });
+	//addMarker({ postion: sf, map: map });
+
+	// const marker = new google.maps.Marker({
+	//     position: sf,
+	//     map: map
+	// });
+	// const infoWindow = new google.maps.InfoWindow({
+	//     content: '<h1>Test Info</h1>'
+	// });
+	// marker.addListener('click', () => {
+	//     infoWindow.open(map, marker);
+	// })
+
+	function addMarker(props) {
+		const marker = new google.maps.Marker({
+			position: props.coords,
+			map: map,
+		});
+
+		if (props.iconImage) {
+			marker.setIcon(props.iconImage);
+		}
+		//check content
+		if (props.content) {
+			const infoWindow = new google.maps.InfoWindow({
+				content: props.content
+			});
+			marker.addListener('mouseover', () => {
+				infoWindow.open(map, marker);
+			});
+			marker.addListener('mouseout', () => {
+				infoWindow.close(map, marker);
+			})
+		}
+	}
+}
