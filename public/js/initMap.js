75f9c94353d712e442b39f0c7bca657606a35407
Alan Li <li.alan011@gmail.com>
2020-04-25 17:38:19 -0700

    attempt #1 to fix errors on map

--- public/js/initMap.js
+++ public/js/initMap.js
@@ -1,7 +1,28 @@
+// document.addEventListener('DOMContentLoaded', () => {
+// 	function initMap() {
+// 		let map = new google.maps.Map(document.getElementById('map'), {
+// 			center: { lat: 37.773, lng: -122.431 },
+// 			zoom: 13
+// 		});
+// 		console.log(map);
+// 	}
+// 	initMap();
+// });
+
+// Create the script tag, set the appropriate attributes
+// var map;
+// var script = document.createElement('script');
+// script.defer = true;
+// script.async = true;
+
 var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 37.773, lng: -122.431 },
-        zoom: 13
-    });
-}
+// Attach your callback function to the `window` object
+window.initMap = function() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: 37.773, lng: -122.431 },
+		zoom: 13
+	});
+};
+
+// Append the 'script' element to 'head'
+// document.head.appendChild(script);
